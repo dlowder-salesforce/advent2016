00c3098b2855b955e5a1fac4e4be6fc08b3e3f43
dlowder-salesforce <dlowder@salesforce.com>
2016-12-16 09:21:22 -0800

    Better day 16 solution

--- advent16.js
+++ advent16.js
@@ -1,34 +1,26 @@
-var initialState = '10000';
+// Example
+var initialState = '10000'.split("").map(function(x) {return parseInt(x);});
 
 var finalLength = 20;
-initialState = '01111001100111011';
+
+initialState = '01111001100111011'.split("").map(function(x) {return parseInt(x);});
 
 // Part 1
 finalLength = 272;
 // Part 2
 finalLength = 35651584;
 
-String.prototype.reverse = function() {
-  var result = "";
-  for (var i=0; i<this.length; i++) {
-    result = result + this.charAt(this.length - 1 - i);
-  }
-  return result;
-};
-
 var nextState = function(a) {
-  return a + '0' + a.reverse().replace(/0/g,'2').replace(/1/g,'0').replace(/2/g,'1');
+  var r = a.map(function(x) {
+    return (x === 0 ? 1 : 0);
+  }).reverse();
+  return a.concat([0].concat(r));
 };
 
 var nextChecksum = function(s) {
-  debugger;
-  var result = '';
+  var result = [];
   for (var i=0; i<(s.length-1); i+=2) {
-    if (s.charAt(i) == s.charAt(i+1)) {
-      result = result + '1';
-    } else {
-      result = result + '0';
-    }
+    result.push( (s[i] == s[i+1]) ? 1 : 0);
   }
   return result;
 };
@@ -39,9 +31,9 @@ while (data.length < finalLength) {
   data = nextState(data);
 }
 
-data = data.substring(0, finalLength);
+data = data.slice(0, finalLength);
 
-console.log(data);
+console.log(data.join(""));
 
 checksum = nextChecksum(data);
 
@@ -49,4 +41,4 @@ while (checksum.length % 2 != 1) {
   checksum = nextChecksum(checksum);
 }
 
-console.log(checksum);
+console.log(checksum.join(""));
